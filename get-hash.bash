a9b6ca4097cec12e81f527089ea3aa56db8e2709
tinekry <ssds95082@gmail.com>
2026-06-27 20:44:15 +0400

    2 more

---
+++ get-hash.bash
@@ -0,0 +1,1 @@
+Get-FileHash "C:\path\to\your\file.ece" -Algorithm SHA256 | Format-List
---
+++ py-math.py
@@ -0,0 +1,4 @@
+import math
+fsin = lambda x: math.sin(math.radians(x))
+fcos = lambda x: math.cos(math.radians(x))
+ftan = lambda x: math.tan(math.radians(x))
